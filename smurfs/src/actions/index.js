dee4315aa27d3d8335f0693ce5c888fabd69ebcd
Vincent Sanders <vincesanders@hotmail.com>
2020-01-24 10:39:23 -0600

    Added axios call in sendData action to add a new smurf to the village.

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -5,8 +5,16 @@ export const SEND_DATA_START = "SEND_DATA_START";
 
 export const fetchData = () => dispatch => {
     dispatch({ type: FETCH_DATA_START });
+    
 }
 
 export const sendData = smurf => dispatch => {
     dispatch({ type: SEND_DATA_START });
+    axios
+    .post('http://localhost:3333/smurfs', smurf)
+    .then(res => {
+        //res.data is the new array of smurfs
+        console.log(res.data)
+    })
+    .catch(err => console.log(err));
 }
