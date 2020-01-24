b20a468e6bb8aea1fd760c43104120ab78962327
Vincent Sanders <vincesanders@hotmail.com>
2020-01-24 10:28:25 -0600

    Added inputs and button to NewSmurfForm with logic to keep track of local state.

--- smurfs/src/components/NewSmurfForm.js
+++ smurfs/src/components/NewSmurfForm.js
@@ -1,9 +1,30 @@
 import React, { useState } from 'react';
 
 export default () => {
+    const [name, setName] = useState('');
+    const [age, setAge] = useState(0);
+    const [height, setHeight] = useState(0);
+
+    const handleChanges = e => {
+        if (e.target.name === 'name') {
+            setName(e.target.value);
+        } else if (e.target.name === 'age') {
+            setAge(e.target.value);
+        } else {
+            setHeight(e.target.value);
+        }
+    }
+
+    const addNewSmurf = e => {
+        console.log('I was clicked!');
+    }
+
     return (
         <div>
-            I'm your smurf form, yo!
+            <input type='text' name='name' onChange={handleChanges} value={name} />
+            <input type='number' name='age' onChange={handleChanges} value={age} />
+            <input type='number' name='height' onChange={handleChanges} value={height} />
+            <button onClick={addNewSmurf} >Add Smurf</button>
         </div>
     );
 }
