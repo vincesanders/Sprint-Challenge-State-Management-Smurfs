bac0a96972adad3ae0832c843d555e2aee26f471
Vincent Sanders <vincesanders@hotmail.com>
2020-01-24 10:20:56 -0600

    Added isLoading to initialState. Added switch statement to reducer with initial and default case.

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+export const FETCH_DATA_START = "FETCH_DATA_START";
+
 export const fetchData = () => dispatch => {
-    
+    dispatch({ type: FETCH_DATA_START });
 }
--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,7 +1,19 @@
-const initialState = {
+import {
+    FETCH_DATA_START
+  } from "../actions";
 
+const initialState = {
+    isLoading: false
 }
 
 export const reducer = (state = initialState, action) => {
-    return state;
+    switch (action.type) {
+        case FETCH_DATA_START:
+            return {
+                ...state,
+                isLoading: true
+            }
+        default:
+            return state;
+    }
 }
